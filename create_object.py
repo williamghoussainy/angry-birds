6b66d2e479a515a960d1d0453023374e462d1dcb
William <william@ghoussainy.com>
2025-08-06 00:08:56 +0100

    feat: added gravity, air resistance to objects, as well as collisions with walls

--- create_object.py
+++ create_object.py
@@ -8,8 +8,39 @@ class Object():
         self.speed = speed
         self.screen = screen
     
-    
     def move(self):
+        # Check for collisions
         self.position[0] += self.speed[0]
         self.position[1] += self.speed[1]
+        
+        if self.position[0] < 0 or self.position[0] + self.size[0] > self.screen.size[0]:
+            if self.position[0] < 0:
+                self.position[0] = 0
+            else:
+                self.position[0] = self.screen.size[0] - self.size[0]
+                
+            self.speed[0] *= -0.9 # 0.9 instead of 1 to simulate the energy lost on bounce
+            
+        if self.position[1] < 0 or self.position[1] + self.size[1] > self.screen.size[1]:
+            if self.position[1] < 0:
+                self.position[1] = 0
+            else:
+                self.position[1] = self.screen.size[1] - self.size[1]
+            
+            self.speed[1] *= -0.9 # 0.9 instead of 1 to simulate the energy lost on bounce
+            
+        self.apply_gravity()
+        self.apply_air_resistance()
+        
+        
+        
+        
+    def apply_gravity(self):
+        self.speed[1] += 9.81 / 60
+        
+    def apply_air_resistance(self):
+        if self.speed[0] > 0:
+            self.speed[0] -= 0.1/60
+        else:
+            self.speed[0] += 0.1/60
 
--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from screen import Screen
 from create_object import Object
 
 if __name__ == "__main__":
-    screen = Screen([600, 600])
+    screen = Screen([800, 600])
     
-    obj1 = Object("Pig", [10, 10], [40, 40], [4, 4], screen)
+    obj1 = Object("Pig", [60, 40], [100, 100], [4, 4], screen)
     
     screen.add_to_screen(obj1)
     
